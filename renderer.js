f17f3ea0e84e9465cef6be12d75f664febd18977
acharushin <acharushin81@gmail.com>
2018-08-23 10:07:43 +1000

    done

--- renderer.js
+++ renderer.js
@@ -2,9 +2,28 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-$ = require('jquery')
+// $ = require('jquery')
+$ = jQuery = require('jquery')
+require('jquery-ui')
+require('jquery-autocomplete')
 require('popper.js')
 require('bootstrap')
+
+var list = $(".friends-list .list-name");
+var dataList = '';
+
+
+var availableTags = new Array();
+for (var i = 0; i < list.length; i ++) {
+	dataList += list[i].innerHTML
+	if (i != list.length - 1)
+		dataList += ','
+}
+$('#search-input').attr('data-list', dataList);
+
+$( "#user-search" ).autocomplete({
+  source: availableTags
+});
 
 var currectSidebar;
 
@@ -37,13 +56,15 @@ btn.addEventListener('click', function (e) {
     e.preventDefault();
     var contents = remote.getCurrentWindow().webContents.getOwnerBrowserWindow().getBounds();
     ipcRenderer.send('resize', contents.width + 345, contents.height);
-    $('.quick-message-board').show();
+    $('.quick-message-board').fadeIn();
     $('#quick-message').attr('show', true)
 	$('.chat-board .quick-message').css('right', '365px')
 	$('.chat-box').css('right', '345px')
 	$('.chat-type').css('right', '545px')
 	$('.chat-board').css('right', '345px')
 	$('.chat-contents').css('right', '365px')
+	$('.send-button').css('right', '375px')
+	$('.smile-icon').css('right', '475px')
 });
 
 function onQuickMessageClose() {
@@ -74,6 +95,42 @@ function onGroup() {
 	$('.user-button').css('border-right', 'none')
 }
 
+function onPerson() {
+	$('.friends-list li').each(function( index ) {
+	  $( this ).removeClass('selected');
+	});
+
+	$(this).addClass('selected');
+}
+
+function onPersonAction() {
+	$('.person-option .dropdown-menu').each(function(index) {
+		$(this).removeClass('show');
+		$(this).attr('style', '');
+	})
+}
+
+$('body').on('click', function() {
+	$('.person-option .dropdown-menu').each(function(index) {
+		$(this).removeClass('show');
+		$(this).attr('style', '');
+	})
+
+	$('.navigation .dropdown-menu').removeClass('show');
+	$('.navigation .dropdown-menu').attr('style', '');
+})
+
+function onMsgSend()
+{
+	
+}
+
+$('.send-button').on('click', onMsgSend)
+
+$('.friends-list li .list-more').on('click', onPersonAction)
+$('.friends-list li').on('click', onPerson)
+$('.friends-list li').on('click', onPerson)
+
 $('.user-button').on('click', onUser)
 $('.group-button').on('click', onGroup)
 
@@ -81,7 +138,45 @@ $('.quick-message-close').on('click', onQuickMessageClose)
 $('img.menu-close').on('click', onMenuClose)
 
 $('#profile').on('click', onProfile)
-$('#group').on('click', onProfile)
-$('#contacts').on('click', onProfile)
 $('#settings').on('click', onSettings)
-$('#about').on('click', onProfile)
+
+
+$('.dropdown').on('show.bs.dropdown', function() {
+	$(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+});
+
+function onAttachment() {
+	$('#attachment').trigger('click')
+}
+
+
+$('.attachment-file').on('click', onAttachment)
+$('.send-button').on('click', function() {
+	var text = $('.chat-input input').val()
+	if (text == '')
+		return
+	$('.chat-input input').val('')
+	var html = '<div class="clearfix"></div>'
+    html += '<div class="row chat-content float-right">'
+    html += '<div class="callout left middle">'
+    html += '<span>' + text + '</span>'
+    html += '</div>'
+    html += '<div class="chat-avatar text-center ">'
+    html += '<img src="imgs/avatar.png" width="34px" height="34px">'
+    html += '<span>12:47</span>'
+    html += '</div>'
+    $('.chat-contents').append(html);
+	
+	var height = $('.chat-contents').prop('scrollHeight');
+	
+    $('.chat-contents').scrollTop(height - $('.chat-contents').height() +  100);
+})
+
+
+$('.profile-content-button').on('click', function() {
+	console.log($('.profile-content.status').innerHTML);
+	$('.profile-content-input').val($('.profile-content.status').innerHTML)
+	// $('.profile-content.status').hide();
+	$('.profile-content-input').show()
+	$('.profile-content-input').focus()
+})
